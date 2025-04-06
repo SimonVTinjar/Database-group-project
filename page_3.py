3ec61f7f974920bf4f52503dc70c0d0378a684b7
Lars Christer Molteberg <larsmolt@stud.ntnu.no>
2025-04-07 00:34:59 +0200

    Added CREATE to restaurant

--- page_3.py
+++ page_3.py
@@ -1,26 +1,71 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import datetime
+from datetime import time, timedelta
+
+
+from db import get_connection
 
 st.write("Hello World")
+st.subheader("Restaurants")
 
-#Basic tabellen
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
+#################### Sets up menu table######################
+conn = get_connection()
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM restaurants")
+rows = cursor.fetchall()
+column_names = [desc[0] for desc in cursor.description]
+cursor.close()
+conn.close()
+##############################################################
 
-df
 
-#Random tabell med randome gule markeringer
-dataframe = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
+if "show_form" not in st.session_state:
+    st.session_state.show_form = False
 
-st.dataframe(dataframe.style.highlight_max(axis=0))
+if st.button("Add New Restaurant:"):
+    st.session_state.show_form = not st.session_state.show_form
 
-# Vet ikke, var i tutorialen
-tabletry = pd.DataFrame(
-    np.random.randn(10, 20),
-    columns=('col %d' % i for i in range(20)))
-st.table(tabletry)
+if st.session_state.show_form:
+
+    st.subheader("Add New Restaurant")
+
+    with st.form("add_new_restaurant"):
+        rName = st.text_input("Restaurant Name")
+        address = st.text_input("Address")
+        phoneNr = st.text_input("Phone number")
+        openingTime = st.time_input("Opening Time", value=datetime.time(10, 0))
+        closingTime = st.time_input("Closing Time", value = datetime.time(22, 0))
+        submitted = st.form_submit_button("Add Restaurant")
+
+        if submitted:
+            if not rName.strip() or not address.strip() or not phoneNr.strip():
+                st.error("Please fill out all fields")
+            else:
+                conn = get_connection()
+                cursor = conn.cursor()
+                cursor.execute("""
+                INSERT INTO restaurants (rName, address, phoneNr, openingTime, closingTime)
+                VALUES (%s, %s, %s, %s, %s)
+                """, (rName, address, phoneNr, openingTime, closingTime))
+                conn.commit()
+                cursor.close()
+                conn.close()
+                st.success(f"'{rName}' added successfully!")
+
+# Format TIME fields as strings
+formatted_rows = []
+for row in rows:
+    row = list(row)
+    for i, value in enumerate(row):
+        if isinstance(value, (time, timedelta)):
+            try:
+                row[i] = value.strftime("%H:%M")
+            except:
+                row[i] = str(value)
+    formatted_rows.append(row)
+
+# Show table
+df = pd.DataFrame(formatted_rows, columns=column_names)
+st.table(df)
